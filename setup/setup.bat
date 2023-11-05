91aa21332acc757fe831dcd39eea31834eadd5d7
David Klingler <deweydbb@gmail.com>
2023-11-05 16:20:53 -0600

    got jar working

--- setup/setup.bat
+++ setup/setup.bat
@@ -1,18 +1,17 @@
 @echo off
 set curdir=%~dp0
-if "%1" == "dummy" goto setupDummy
 
 for %%i in (1 2 3) do (
     mkdir "%curdir%\server%%i"
     copy /Y init-cluster.json "%curdir%\server%%i\cluster.json"
     echo server.id=%%i> "%curdir%\server%%i\config.properties"
     echo start server%%i
-    start "server%%i" /D "%curdir%\server%%i" java -jar %curdir%\dmprinter.jar server "%curdir%\server%%i" 800%%i
+    start "server%%i" /D "%curdir%\server%%i" java -jar %curdir%\kvstore.jar server "%curdir%\server%%i" 800%%i
 )
 
 echo start a client
 mkdir client
 copy /Y init-cluster.json "%curdir%\client\cluster.json"
 copy /Y "%curdir%\server1\config.properties" "%curdir%\client\config.properties"
-start "client" /D "%curdir%\client" java -jar %curdir%\dmprinter.jar client "%curdir%\client"
+start "client" /D "%curdir%\client" java -jar %curdir%\kvstore.jar client "%curdir%\client"
 @echo on
